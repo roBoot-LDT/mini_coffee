498f78b1af948ebbbf49125fcca2dc51f72ab437
roBoot <maksim.nemkov.ldt@gmail.com>
2025-07-30 22:52:17 +0300

    fix

--- src/mini_coffee/gui/client/client_screen.py
+++ src/mini_coffee/gui/client/client_screen.py
@@ -39,9 +39,8 @@ class ClientScreen(QWidget):
 
     def check_serial_port(self):
         try:
-            line = self.ser.readline()
+            print(self.ser.readline())
             # if value == 1:
-            print(f"Получено: {line}")
                     # You can add any logic here (e.g., trigger an action)
                 
         except Exception as e:
